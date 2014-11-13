17e93456ae62ea6c334058667c12855f2fc51ccb
alvian <alviandk@gmail.com>
2014-11-13 15:44:20 +0700

    selesai ppm part 1

---
+++ inventory/migrations/0001_initial.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Item',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+                ('brand', models.CharField(max_length=200, null=True, blank=True)),
+                ('description', models.TextField()),
+                ('added_date', models.DateTimeField(auto_now_add=True)),
+                ('added_by', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Location',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='item',
+            name='location',
+            field=models.ForeignKey(related_name='items', to='inventory.Location'),
+            preserve_default=True,
+        ),
+    ]
